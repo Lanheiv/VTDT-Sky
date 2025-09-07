356a089c71f487706fd36fae9f0abd76fef2fdae
Lanheiv <markussmpp@gmail.com>
2025-09-07 17:24:50 +0300

    box 1 add basic

--- index.php
+++ index.php
@@ -13,102 +13,121 @@
 <body class="light"> <!-- light / dark -->
     <div class="root">
         <div class="data-box">
-            <header>
-                <div class="left-hedbox">
+            <header class="flex c-box c-shadows">
+                <div class="left-hedbox flex">
                     <button>
-                        <svg viewBox=" 24" xmlns="http://www.w3.org/2000/svg">
+                        <svg viewBox=" 24" xmlns="http://www.w3.org/2000/svg" class="icon">
                             <path d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" stroke-linecap="round"></path>
                         </svg>
                     </button>
 
                     <h1>VTDT Sky</h1>
-                    <img src="./assets/google-maps.gif" alt="google map gif">
+                    <img src="./assets/google-maps.gif" alt="google map gif" class="icon">
                     <span><?php echo $data['city']['name'].", ".$data['city']['country']; ?></span>
                 </div>
-                <div class="center-hedbox">
-                    <div class="serch-div">
-                        <form>
+                <div class="center-hedbox flex-rel">
+                    <div class="serch-div flex-rel">
+                        <form class="flex">
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M11 2a9 9 0 1 0 6.293 15.293l4.707 4.707a1 1 0 0 0 1.414-1.414l-4.707-4.707A9 9 0 0 0 11 2zM11 4a7 7 0 1 1 0 14 7 7 0 0 1 0-14z"></path>
                             </svg>
                             <input type="text" placeholder="Search Location" value="cesis">
                         </form>
-                        <img src="./assets/worldwide.gif" alt="worldwide gif">
+                        <img src="./assets/worldwide.gif" alt="worldwide gif" class="icon">
                     </div>
 
-                    <button>
+                    <button class="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"></path>
                         </svg>
                         <span>Light</span>
                     </button>
                 </div>
-                <div class="right-hedbox">
-                    <button>
+                <div class="right-hedbox flex">
+                    <button class="c-box">
                         <img src="./assets/notification.gif" alt="notification gif">
                     </button>
 
-                    <button>
+                    <button class="c-box">
                         <img src="./assets/settings.gif" alt="settings gif" style="margin-left: 1rem;">
                     </button>
                 </div>
             </header>
             <main>
-                <div class="box box-1">
-                    test 1
+                <div class="box c-box c-shadows box-1">
+                    <div>
+                        <div style="font-size: .75rem; line-height: 1.25rem;">Current Weather</div>
+                        <div style="font-weight: 500; font-size: 1.25rem; line-height: 1.75rem;">Local time: 5:08 PM</div>
+                        <div class="flex">
+                            <img src="./assets/humidity.gif" alt="clouds" style="width: 3rem; height: 3rem;">
+                            <div style="font-weight: 600; font-size: 3rem; line-height: 1; padding-left: .75rem;">24.7</div>
+                            <div style="font-weight: 600; font-size: 1.5rem; line-height: 2rem; padding-right: .5rem; margin-bottom: .5rem;">°C</div>
+                            <div class="flex" style="font-size: .875rem; line-height: 1.25rem; padding-left: 1.5rem; flex-direction: column; align-items: normal;">
+                                <div>Partly Cloudy</div>
+                                <div>Feels Like 25.7°C</div>
+                            </div>
+                        </div>
+                    </div>
+                    <p style="margin-top: 3rem;">Current wind direction: SE</p>
                 </div>
-                <div class="box box-2">
+                <div class="box c-box c-shadows box-2">
                     test 2
                 </div>
-                <div class="box">
-                    <div style="align-items: center; display: flex;">
-                        <img src="./assets/humidity.gif" alt="clouds" style="height: 1.5rem; width: 1.5rem;">
-                        <span style="margin-left: .5rem;">Humidity</span>
+                <div class="box c-box c-shadows">
+                    <div class="flex">
+                        <img src="./assets/humidity.gif" alt="clouds" class="icon">
+                        <span class="s-text">Humidity</span>
                     </div>
                     <span style="margin-left: 2rem; font-weight: 600; font-size: 1.5rem; line-height: 2rem;">
                         <?php echo $data["list"]["0"]["humidity"]."%"; ?>
                     </span>
                 </div>
-                <div class="box">
-                    <div style="align-items: center; display: flex;">
-                        <img src="./assets/air-pump.gif" alt="air-pump" style="height: 1.5rem; width: 1.5rem;">
-                    <span style="margin-left: .5rem;">Pressure</span>
+                <div class="box c-box c-shadows">
+                    <div class="flex">
+                        <img src="./assets/air-pump.gif" alt="air-pump" class="icon">
+                    <span class="s-text">Pressure</span>
                     </div>
                     <span style="margin-left: 2rem; font-weight: 600; font-size: 1.5rem; line-height: 2rem;">
                         <?php echo $data["list"]["0"]["pressure"]."°"; ?>
                     </span>
                 </div>
-                <div class="box">
-                    <div style="align-items: center; display: flex;">
-                        <img src="./assets/air-pump.gif" alt="air-pump" style="height: 1.5rem; width: 1.5rem;">
-                    <span style="margin-left: .5rem;">Precipitation</span>
+                <div class="box c-box c-shadows">
+                    <div class="flex">
+                        <img src="./assets/clouds.gif" alt="clouds" class="icon">
+                    <span class="s-text">Precipitation</span>
                     </div>
                     <span style="margin-left: 2rem; font-weight: 600; font-size: 1.5rem; line-height: 2rem;">
                         <?php echo (float) $data["list"]["0"]["pop"]*100 ."%"; ?>
                     </span>
                 </div>
-                <div class="box">
-                    <div style="align-items: center; display: flex;">
-                        <img src="./assets/air-pump.gif" alt="air-pump" style="height: 1.5rem; width: 1.5rem;">
-                    <span style="margin-left: .5rem;">Pressure</span>
+                <div class="box c-box c-shadows">
+                    <div class="flex">
+                        <img src="./assets/clouds.gif" alt="clouds" class="icon">
+                    <span class="s-text">Clouds</span>
                     </div>
                     <span style="margin-left: 2rem; font-weight: 600; font-size: 1.5rem; line-height: 2rem;">
-                        <?php echo $data["list"]["0"]["pressure"]."°"; ?>
+                        <?php echo $data["list"]["0"]["clouds"]."%"; ?>
                     </span>
                 </div>
-                <div class="box">
-                    <div style="align-items: center; display: flex;">
-                        <img src="./assets/air-pump.gif" alt="air-pump" style="height: 1.5rem; width: 1.5rem;">
-                    <span style="margin-left: .5rem;">Pressure</span>
+                <div class="box c-box c-shadows">
+                    <div class="flex">
+                        <img src="./assets/wind.gif" alt="wind" class="icon">
+                    <span class="s-text">Max Wind Speed</span>
                     </div>
                     <span style="margin-left: 2rem; font-weight: 600; font-size: 1.5rem; line-height: 2rem;">
-                        <?php echo $data["list"]["0"]["pressure"]."°"; ?>
+                        <?php echo $data["list"]["0"]["gust"]."m/s"; ?>
                     </span>
                 </div>
-                <div class="box">
-                    test
+                <div class="box c-box c-shadows">
+                    <div class="flex">
+                        <img src="./assets/wind.gif" alt="wind" class="icon">
+                    <span class="s-text">Average Wind Speed</span>
+                    </div>
+                    <span style="margin-left: 2rem; font-weight: 600; font-size: 1.5rem; line-height: 2rem;">
+                        <?php echo $data["list"]["0"]["speed"]."m/s"; ?>
+                    </span>
                 </div>
-                <div class="box box-9">
+                <div class="box c-box c-shadows box-9">
                     test 9
                 </div>
             </main>
